aa38c91ad58e0a0fbcc1d02c6fca398b0f9d5a93
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-03-13 11:43:37 +0700

    [#4] Implement the SignIn screen

---
+++ lib/theme/dimens.dart
@@ -0,0 +1,11 @@
+const double space10 = 10.0;
+const double space12 = 12.0;
+const double space14 = 14.0;
+const double space16 = 16.0;
+const double space18 = 18.0;
+const double space20 = 20.0;
+
+const double borderRadius10 = 10.0;
+const double borderRadius12 = 12.0;
+
+const double fontSize17 = 17.0;
--- lib/ui/signin/sign_in_screen.dart
+++ lib/ui/signin/sign_in_screen.dart
@@ -1,4 +1,11 @@
-import 'package:flutter/widgets.dart';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:survey_flutter_ic/extension/context_extension.dart';
+import 'package:survey_flutter_ic/gen/assets.gen.dart';
+import 'package:survey_flutter_ic/theme/dimens.dart';
+import 'package:survey_flutter_ic/widget/flat_button_text.dart';
+import 'package:survey_flutter_ic/widget/text_input_field.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
@@ -10,6 +17,87 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(Assets.images.bgSplash.path),
+                opacity: 0.6,
+                fit: BoxFit.fill),
+          ),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+            child: Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.black26, Colors.black])),
+                child: _buildSignInForm()),
+          )),
+    );
+  }
+
+  Widget _buildSignInForm() {
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: space16, vertical: space10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Assets.images.icLogo
+                      .svg(width: 168, height: 40, fit: BoxFit.none),
+                ),
+                Expanded(child: _buildSignInContent()),
+                Expanded(
+                  child: Container(),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildSignInContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextInputField(
+          hintText: context.localization.sign_in_email_label,
+          textInputType: TextInputType.emailAddress,
+          onChanged: (input) => {
+            // TODO: Handle listener: enable/disable SignIn button
+          },
+          onSubmitted: (input) => {},
+        ),
+        const SizedBox(height: space20),
+        TextInputField(
+          hintText: context.localization.sign_in_password_label,
+          isObscureText: true,
+          obscuringCharacter: "●",
+          onChanged: (input) => {
+            // TODO: Handle listener: enable/disable SignIn button
+          },
+          onSubmitted: (input) => {},
+        ),
+        const SizedBox(height: space20),
+        FlatButtonText(
+          text: context.localization.sign_in_button,
+          isEnabled: false,
+          onPressed: () => {
+            // TODO: Integrate ViewModel signIn
+          },
+        )
+      ],
+    );
   }
 }
