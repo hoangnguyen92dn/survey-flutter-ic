3925a17e9ee1e939a6ed8f9e5b0298a430850631
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-03-15 23:50:25 +0700

    [#6] Add UnitTest

--- test/mocks/generate_mocks.dart
+++ test/mocks/generate_mocks.dart
@@ -1,13 +1,17 @@
 import 'package:dio/dio.dart';
-import 'package:survey_flutter_ic/api/api_service.dart';
 import 'package:mockito/annotations.dart';
+import 'package:survey_flutter_ic/api/api_service.dart';
+import 'package:survey_flutter_ic/api/persistence/auth_persistence.dart';
 import 'package:survey_flutter_ic/api/repository/auth_repository.dart';
 import 'package:survey_flutter_ic/api/service/auth_service.dart';
+import 'package:survey_flutter_ic/usecase/sign_in_use_case.dart';
 
 @GenerateMocks([
   ApiService,
   AuthService,
   AuthRepository,
+  AuthPersistence,
+  SignInUseCase,
   DioError,
 ])
 main() {
--- test/usecase/sign_in_use_case_test.dart
+++ test/usecase/sign_in_use_case_test.dart
@@ -1,19 +1,23 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:survey_flutter_ic/api/exception/network_exceptions.dart';
 import 'package:survey_flutter_ic/model/auth_model.dart';
 import 'package:survey_flutter_ic/usecase/base/base_use_case.dart';
 import 'package:survey_flutter_ic/usecase/sign_in_use_case.dart';
+
 import '../mocks/generate_mocks.mocks.dart';
 
 void main() {
   group('SignInUseCase', () {
     late MockAuthRepository mockRepository;
+    late MockAuthPersistence mockPersistence;
     late SignInUseCase useCase;
 
     setUp(() {
       mockRepository = MockAuthRepository();
-      useCase = SignInUseCase(mockRepository);
+      mockPersistence = MockAuthPersistence();
+      useCase = SignInUseCase(mockRepository, mockPersistence);
     });
 
     test('When calling signIn successfully, it returns the result Success',
@@ -28,6 +32,12 @@ void main() {
         email: 'email',
         password: 'password',
       )).thenAnswer((_) async => authModel);
+
+      when(mockPersistence.storeAccessToken(any)).thenAnswer((_) async => {});
+
+      when(mockPersistence.storeRefreshToken(any)).thenAnswer((_) async => {});
+
+      when(mockPersistence.storeTokenType(any)).thenAnswer((_) async => {});
 
       final result = await useCase.call(
         SignInInput(
@@ -56,6 +66,33 @@ void main() {
 
       expect(result, isA<Failed>());
       expect((result as Failed).exception.actualException, exception);
+    });
+
+    test('When store token failed, it returns the result Failed', () async {
+      const authModel = AuthModel(
+        accessToken: 'accessToken',
+        tokenType: 'tokenType',
+        refreshToken: 'refreshToken',
+      );
+
+      when(mockRepository.signIn(
+        email: 'email',
+        password: 'password',
+      )).thenAnswer((_) async => authModel);
+
+      when(mockPersistence.storeAccessToken(any))
+          .thenThrow(PlatformException(code: 'code'));
+
+      final result = await useCase.call(
+        SignInInput(
+          email: 'email',
+          password: 'password',
+        ),
+      );
+
+      expect(result, isA<Failed>());
+      expect((result as Failed).exception.actualException,
+          isA<PlatformException>());
     });
   });
 }
