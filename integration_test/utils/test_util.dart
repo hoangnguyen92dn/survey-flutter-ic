5873d26460e022776f9e4c0d12394cbaf2dffddb
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-03-17 00:39:06 +0700

    [#6] Add integration test

--- integration_test/utils/test_util.dart
+++ integration_test/utils/test_util.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:survey_flutter_ic/main.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:survey_flutter_ic/api/service/auth_service.dart';
+import 'package:survey_flutter_ic/di/provider/di.dart';
+import 'package:survey_flutter_ic/main.dart';
+import 'package:survey_flutter_ic/navigation/route.dart';
+
+import '../fake_data/fake_services/fake_auth_service.dart';
 
 class TestUtil {
   /// This is useful when we test the whole app with the real configs(styling,
@@ -23,6 +28,18 @@ class TestUtil {
     );
   }
 
+  static Widget pumpWidgetWithRoutePath(String route) {
+    _initDependencies();
+
+    return MaterialApp.router(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      routeInformationProvider: router.routeInformationProvider,
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
+    );
+  }
+
   static void _initDependencies() {
     PackageInfo.setMockInitialValues(
         appName: 'Flutter templates testing',
@@ -32,5 +49,12 @@ class TestUtil {
         buildSignature: '',
         installerStore: '');
     FlutterConfig.loadValueForTesting({'SECRET': 'This is only for testing'});
+  }
+
+  static Future setupTestEnvironment() async {
+    _initDependencies();
+    getIt.allowReassignment = true;
+    getIt.registerSingleton<AuthService>(FakeAuthService());
+    configureInjection();
   }
 }
