602bc19a72314683c55d24cb428fd1a3780502f3
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-04-07 16:07:15 +0700

    [#16] Add Survey Details ViewModel

--- lib/ui/details/survey_details_screen.dart
+++ lib/ui/details/survey_details_screen.dart
@@ -5,6 +5,7 @@ import 'package:go_router/go_router.dart';
 import 'package:survey_flutter_ic/extension/context_extension.dart';
 import 'package:survey_flutter_ic/gen/assets.gen.dart';
 import 'package:survey_flutter_ic/theme/dimens.dart';
+import 'package:survey_flutter_ic/ui/details/survey_details_view_model.dart';
 import 'package:survey_flutter_ic/ui/surveys/survey_ui_model.dart';
 import 'package:survey_flutter_ic/widget/flat_button_text.dart';
 
@@ -23,14 +24,21 @@ class SurveyDetailsScreen extends ConsumerStatefulWidget {
 
 class _SurveyDetailsScreenState extends ConsumerState<SurveyDetailsScreen> {
   @override
+  void initState() {
+    ref.read(surveyDetailsViewModelProvider.notifier).setSurvey(widget.survey);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final survey = ref.watch(surveyDetailStream).value;
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
             image: FadeInImage.assetNetwork(
               placeholder: Assets.images.placeholderAvatar.path,
-              image: widget.survey.largeCoverImageUrl.toString(),
+              image: survey?.largeCoverImageUrl ?? '',
             ).image,
             fit: BoxFit.cover,
           ),
@@ -44,53 +52,59 @@ class _SurveyDetailsScreenState extends ConsumerState<SurveyDetailsScreen> {
               icon: SvgPicture.asset(Assets.images.icArrowLeft.path),
               padding: const EdgeInsets.all(space20),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: space20),
-              child: Text(
-                widget.survey.title,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: fontSize34,
-                  fontWeight: FontWeight.w800,
-                ),
-              ),
-            ),
+            _buildSurveyTitle(survey?.title ?? ''),
             const SizedBox(height: space16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: space20),
-              child: Text(
-                widget.survey.description,
-                style: TextStyle(
-                  color: Colors.white.withOpacity(0.7),
-                  fontSize: fontSize17,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ),
+            _buildSurveyDescription(survey?.description ?? ''),
             const Expanded(child: SizedBox.shrink()),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(
-                0,
-                0,
-                space20,
-                space57,
-              ),
-              child: Row(
-                children: [
-                  const Expanded(
-                    child: SizedBox.shrink(),
-                  ),
-                  FlatButtonText(
-                    text:
-                        context.localization.survey_details_start_survey_button,
-                    isEnabled: true,
-                    onPressed: () => {}, // TODO Start survey
-                  ),
-                ],
-              ),
-            ),
+            _buildStartSurveyButton(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildSurveyTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: space20),
+      child: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: fontSize34,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSurveyDescription(String description) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: space20),
+      child: Text(
+        description,
+        style: TextStyle(
+          color: Colors.white.withOpacity(0.7),
+          fontSize: fontSize17,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStartSurveyButton() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 0, space20, space57),
+      child: Row(
+        children: [
+          const Expanded(
+            child: SizedBox.shrink(),
+          ),
+          FlatButtonText(
+            text: context.localization.survey_details_start_survey_button,
+            isEnabled: true,
+            onPressed: () => {}, // TODO Start survey
+          ),
+        ],
       ),
     );
   }
--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:survey_flutter_ic/extension/toast_extension.dart';
+import 'package:survey_flutter_ic/navigation/app_router.dart';
 import 'package:survey_flutter_ic/ui/home/home_header.dart';
 import 'package:survey_flutter_ic/ui/home/home_view_model.dart';
 import 'package:survey_flutter_ic/ui/home/home_widget_id.dart';
@@ -43,6 +45,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
           final profile = ref.watch(profileStream).value;
           final today = ref.watch(todayStream).value;
           final surveys = ref.watch(surveysStream).value ?? [];
+          final visibleIndex = ref.watch(visibleIndexStream).value ?? 0;
           return Stack(
             children: [
               SurveyView(
@@ -53,7 +56,10 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                       .setVisibleSurveyIndex(visibleIndex);
                 },
                 onSurveySelected: (survey) {
-                  // TODO: Navigate to survey details
+                  context.goNamed(
+                    RouteName.details.name,
+                    extra: surveys[visibleIndex],
+                  );
                 },
               ),
               SafeArea(
