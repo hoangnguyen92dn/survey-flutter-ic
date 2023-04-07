77a87553ba086ae8144dd5e6d51dc5ed657b280f
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-04-07 17:03:46 +0700

    [#16] Add ViewModel test

---
+++ test/ui/details/survey_details_view_model_test.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:survey_flutter_ic/ui/details/survey_details_view_model.dart';
+import 'package:survey_flutter_ic/ui/details/survey_details_view_state.dart';
+import 'package:survey_flutter_ic/ui/surveys/survey_ui_model.dart';
+
+void main() {
+  group('SurveyDetailsViewModel', () {
+    late SurveyDetailsViewModel viewModel;
+    late ProviderContainer container;
+
+    setUp(() {
+      container = ProviderContainer(overrides: [
+        surveyDetailsViewModelProvider
+            .overrideWith((ref) => SurveyDetailsViewModel())
+      ]);
+      viewModel = container.read(surveyDetailsViewModelProvider.notifier);
+      addTearDown(() => container.dispose());
+    });
+
+    test('When initializing SurveyDetailsViewModel, it emits Init state', () {
+      expect(container.read(surveyDetailsViewModelProvider), const SurveyDetailsViewState.init());
+    });
+
+    test('When calling setSurvey, it emits survey on surveyDetailStream', () {
+      const survey = SurveyUiModel(
+        id: 'id',
+        title: 'title',
+        description: 'description',
+        isActive: true,
+        coverImageUrl: 'coverImageUrl',
+        largeCoverImageUrl: 'largeCoverImageUrl',
+        createdAt: 'createdAt',
+        surveyType: 'surveyType',
+      );
+
+      expect(
+        container.read(surveyDetailStream.future).asStream(),
+        emits(survey),
+      );
+
+      viewModel.setSurvey(survey);
+    });
+  });
+}
