e400dd2315150f57992b4e482b4bb4096d25bc6c
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-04-11 12:03:19 +0700

    [#20] Navigate to Survey questions from Survey details

--- lib/ui/details/survey_details_screen.dart
+++ lib/ui/details/survey_details_screen.dart
@@ -4,9 +4,11 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:survey_flutter_ic/extension/context_extension.dart';
 import 'package:survey_flutter_ic/gen/assets.gen.dart';
+import 'package:survey_flutter_ic/navigation/app_router.dart';
 import 'package:survey_flutter_ic/theme/dimens.dart';
 import 'package:survey_flutter_ic/ui/details/survey_details_view_model.dart';
 import 'package:survey_flutter_ic/ui/details/survey_details_widget_id.dart';
+import 'package:survey_flutter_ic/ui/questions/survey_questions_screen.dart';
 import 'package:survey_flutter_ic/ui/surveys/survey_ui_model.dart';
 import 'package:survey_flutter_ic/widget/flat_button_text.dart';
 
@@ -76,7 +78,7 @@ class _SurveyDetailsScreenState extends ConsumerState<SurveyDetailsScreen> {
           const SizedBox(height: space16),
           _buildSurveyDescription(survey.description),
           const Expanded(child: SizedBox.shrink()),
-          _buildStartSurveyButton(),
+          _buildStartSurveyButton(survey),
         ],
       ),
     );
@@ -112,22 +114,27 @@ class _SurveyDetailsScreenState extends ConsumerState<SurveyDetailsScreen> {
     );
   }
 
-  Widget _buildStartSurveyButton() {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(0, 0, space20, space57),
-      child: Row(
-        children: [
-          const Expanded(
-            child: SizedBox.shrink(),
-          ),
-          FlatButtonText(
-            key: SurveyDetailsWidgetId.startSurveyButton,
-            text: context.localization.survey_details_start_survey_button,
-            isEnabled: true,
-            onPressed: () => {}, // TODO Start survey
-          ),
-        ],
-      ),
-    );
-  }
+  Widget _buildStartSurveyButton(SurveyUiModel survey) => Consumer(
+        builder: (context, _, __) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, space20, space57),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FlatButtonText(
+                  key: SurveyDetailsWidgetId.startSurveyButton,
+                  text: context.localization.survey_details_start_survey_button,
+                  isEnabled: true,
+                  onPressed: () => {
+                    context.goNamed(
+                      RoutePath.questions.routeName,
+                      params: {surveyIdKey: survey.id},
+                    ),
+                  },
+                ),
+              ],
+            ),
+          );
+        },
+      );
 }
