5627f861b47e906f5abf14a8380944e32b02f208
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-04-13 15:01:54 +0700

    [#24] Bind emojis on rating UI

--- lib/ui/questions/survey_question_view.dart
+++ lib/ui/questions/survey_question_view.dart
@@ -78,24 +78,34 @@ class _SurveyQuestionViewState extends ConsumerState<SurveyQuestionView> {
     );
   }
 
-  Widget _buildAnswers(SurveyQuestionUiModel question) {
-    switch (question.displayType) {
-      case QuestionDisplayType.dropdown:
-        return AnswerDropdown(
-          key: SurveyQuestionsWidgetId.answersDropdown,
-          answers: question.answers,
-        );
-      case QuestionDisplayType.star:
-      case QuestionDisplayType.heart:
-      case QuestionDisplayType.smiley:
-        return AnswerEmoji(
-          displayType: question.displayType,
-          answers: question.answers,
-        );
-      default:
-        return const SizedBox.shrink();
-    }
-  }
+  Widget _buildAnswers(SurveyQuestionUiModel question) =>
+      Consumer(builder: (_, ref, __) {
+        switch (question.displayType) {
+          case QuestionDisplayType.dropdown:
+            return AnswerDropdown(
+              key: SurveyQuestionsWidgetId.answersDropdown,
+              answers: question.answers,
+            );
+          case QuestionDisplayType.star:
+          case QuestionDisplayType.heart:
+          case QuestionDisplayType.smiley:
+            return AnswerEmoji(
+              key: SurveyQuestionsWidgetId.answersRating,
+              displayType: question.displayType,
+              answers: question.answers,
+              onAnswerSelected: (answer) {
+                ref
+                    .read(surveyQuestionsViewModelProvider.notifier)
+                    .selectAnswer(
+                      question.id,
+                      answer.id,
+                    );
+              },
+            );
+          default:
+            return const SizedBox.shrink();
+        }
+      });
 
   void nextPage() {
     _pageController.nextPage(
